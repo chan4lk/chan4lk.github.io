48301f946b77c3b98dd97f9bc0497ffb7bf51266
Chandima Ranaweera <chan4lk@gmail.com>
2020-02-15 16:21:04 +0530

    change cv path

--- src/portfolio.js
+++ src/portfolio.js
@@ -5,7 +5,7 @@ const greeting = {
   title: "Hi all 👋 I'm Chandima",
   subTitle:
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks",
-  resumeLink: "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing"
+  resumeLink: "https://drive.google.com/open?id=1W6MK7QykwUhSwVTjR0XsmyTvXBO3B442"
 };
 
 const socialMediaLinks = {
