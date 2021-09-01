aa45381997e6e6af905106e0e87f156ae977d697
Chandima Ranaweera <chan4lk@gmail.com>
2021-09-01 12:09:01 +0530

    Fix build

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -6,64 +6,61 @@ import GithubRepoCard from '../../components/githubRepoCard/GithubRepoCard';
 import Button from '../../components/button/Button';
 import { openSource } from '../../portfolio';
 
+
+
 export default function Projects() {
   const [repo, setrepo] = useState([]);
 
   useEffect(() => {
-    getRepoData();
-  }, []);
-
-  function getRepoData() {
-    const client = new ApolloClient({
-      uri: 'https://api.github.com/graphql',
-      request: (operation) => {
-        operation.setContext({
-          headers: {
-            authorization: `Bearer ${window.atob(process.env.REACT_APP_TOKEN)}`,
-          },
-        });
-      },
-    });
-
-    client
-      .query({
-        query: gql`
-          {
-            repositoryOwner(login: "${openSource.githubUserName}") {
-              ... on User {
-                repositories(first: 7, orderBy: {field:UPDATED_AT, direction:DESC} ) {
-                  edges {
-                    node {
-                      nameWithOwner
-                      description
-                      forkCount
-                      stargazers {
-                        totalCount
-                      }
-                      url
-                      id
-                      diskUsage
-                      primaryLanguage {
-                        name
-                        color
+    (function getRepoData() {
+      const client = new ApolloClient({
+        uri: 'https://api.github.com/graphql',
+        request: (operation) => {
+          operation.setContext({
+            headers: {
+              authorization: `Bearer ${window.atob(process.env.REACT_APP_TOKEN)}`,
+            },
+          });
+        },
+      });
+  
+      client
+        .query({
+          query: gql`
+            {
+              repositoryOwner(login: "${openSource.githubUserName}") {
+                ... on User {
+                  repositories(first: 7, orderBy: {field:UPDATED_AT, direction:DESC} ) {
+                    edges {
+                      node {
+                        nameWithOwner
+                        description
+                        forkCount
+                        stargazers {
+                          totalCount
+                        }
+                        url
+                        id
+                        diskUsage
+                        primaryLanguage {
+                          name
+                          color
+                        }
                       }
                     }
                   }
                 }
               }
             }
-          }
-        `,
-      })
-      .then((result) => {
-        setrepoFunction(result.data.repositoryOwner.repositories.edges);
-        console.log(result);
-      });
-  }
+          `,
+        })
+        .then((result) => {
+          setrepo(result.data.repositoryOwner.repositories.edges);
+        });
+    })();
 
-  function setrepoFunction(array) {
-    setrepo(array);
-  }
+  }, []);
+
 
   return (
     <div className="main" id="opensource">
